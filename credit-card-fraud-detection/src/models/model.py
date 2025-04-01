19d72f3c7c5f63cbe1ba07fa9ba470bc9c6e43f0
chima1968 <chimarokeumunna98@gmail.com>
2025-04-01 16:22:43 +0100

    update

--- credit-card-fraud-detection/src/models/model.py
+++ credit-card-fraud-detection/src/models/model.py
@@ -14,3 +14,11 @@ class Transaction(BaseModel):
 
 class FraudDetectionInput(BaseModel):
     transactions: List[Transaction]
+
+class FraudDetectionOutput(BaseModel):
+    transaction_id: str
+    is_fraud: bool
+    fraud_score: Optional[float] = None
+
+class FraudDetectionBatchOutput(BaseModel):
+    results: List[FraudDetectionOutput]
